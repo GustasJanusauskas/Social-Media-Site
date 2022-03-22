6cd87294d583a58556d58d8600410801117356dc
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2022-03-22 16:07:50 +0200

    Moved frontend from React to Angular

---
+++ _DB Setup.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE socialsitedb;
+\c socialsitedb;
+
+CREATE TABLE users(
+	usr_id bigserial UNIQUE NOT NULL,
+	username varchar(64) UNIQUE NOT NULL,
+	password varchar(128) NOT NULL,
+	email varchar(256) UNIQUE NOT NULL,
+	created timestamp,
+	salt varchar(8),
+	pepper varchar(8),
+	PRIMARY KEY(usr_id)
+);
+
+CREATE TABLE profiles(
+	usr_id bigint UNIQUE NOT NULL,
+	firstname varchar(128),
+	lastname varchar(128),
+	description varchar(1024),
+	picture varchar(256),
+	friends bigint[],
+	posts varchar(4096)[]
+);
+
+CREATE TABLE messages(
+	msg_id bigserial UNIQUE NOT NULL,
+	usr_id bigint UNIQUE NOT NULL,
+	msg varchar(300) NOT NULL,
+	recipient bigint NOT NULL,
+	date timestamp,
+	ip varchar(45),
+	file varchar(256),
+	PRIMARY KEY(msg_id)
+);
--- server/index.js
+++ server/index.js
@@ -1,16 +1,76 @@
+'use strict';
+
+const fs = require('fs');
+const ws = require('ws');
+const { Client } = require('pg');
+const sanitizeHtml = require('sanitize-html');
+const crypto = require('crypto');
 const express = require("express");
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const linkRegex = new RegExp(String.raw`[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$`);
+const base64Regex = new RegExp(String.raw`^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$`);
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+const dbclient = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'socialsitedb',
+  password: 'root',
+  port: 5432
+}); dbclient.connect();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
+  RegisterUser('TestUser','TestPass','TestEmail@email.com');
   res.json({ message: "Hello from server!" });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+function RegisterUser(user,passw,email) {
+  if (!email || !user || !passw) {
+    //toSend[ip].send("Make sure all the fields are filled.");
+    return;
+  }
+
+  if (email.length > 256 || user.length > 64) {
+    //toSend[ip].send("Username or email is too long.");
+    return;
+  }
+
+  if(!emailRegex.test(email.toLowerCase())) {
+    //toSend[ip].send("Email invalid.");
+    return;
+  }
+
+  var passSalt = RandomString(8);
+  var passPepper = RandomString(8);
+
+  var finalPass = crypto.createHash('BLAKE2b512').update(passSalt + passw + passPepper).digest('hex');
+  var query = 'INSERT INTO users(username,password,email,created,salt,pepper) VALUES($1,$2,$3,NOW(),$4,$5)';
+  var data = [user,finalPass,email,passSalt,passPepper];
+
+  dbclient.query(query,data, (err, res) => {
+    if (err) {
+        //toSend[ip].send("Account already exists.");
+        console.log("DB ERROR: \n" + err);
+    }
+    else return;//toSend[ip].send("Account created!");
+  });
+}
+
+function RandomString(length) {
+  var pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var result = "";
+  for (var x = 0; x < length;x++) {
+    result += pool.charAt(Math.random() * pool.length - 1);
+  }
+  return result;
+}
